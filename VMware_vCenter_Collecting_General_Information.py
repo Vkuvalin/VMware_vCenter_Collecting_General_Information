5dc1a3f996c1fda49456fddb3e87469fbe7cd00f
Кувалин Влад <59653793+Vkuvalin@users.noreply.github.com>
2022-05-26 23:45:13 +0300

    Update VMware_vCenter_Collecting_General_Information.py

--- VMware_vCenter_Collecting_General_Information.py
+++ VMware_vCenter_Collecting_General_Information.py
@@ -1,11 +1,3 @@
-"""
-Данный скрипт:
-Получает данные о виртуальных машинах из vCentr'a через restapi,
-форматирует и приводи их в удобный для загрузки формат.
-Создает КЕ node и vmware_host_resource и загружает все в UCMDB
-(inventory discovery).
-"""
-
 # coding=utf-8
 import rest_requests as requests
 import rest_json as json
